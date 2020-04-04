981c5c85b66d2528593d3b1a1b392ec9008298d5
PashkaBro <veenoompoltava@gmail.com>
2020-04-04 11:22:04 -0700

    Updates

---
+++ precache-manifest.2791834665ffa6d1cc116884f56cfd99.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82fc59149ab49a9598e6196bb3f3721f",
+    "url": "/React-FE12/index.html"
+  },
+  {
+    "revision": "e674b3277c87fcf45dc9",
+    "url": "/React-FE12/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "ed63c0c005a24da1086f",
+    "url": "/React-FE12/static/css/main.c8c20720.chunk.css"
+  },
+  {
+    "revision": "e674b3277c87fcf45dc9",
+    "url": "/React-FE12/static/js/2.490f583d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/React-FE12/static/js/2.490f583d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed63c0c005a24da1086f",
+    "url": "/React-FE12/static/js/main.78280b04.chunk.js"
+  },
+  {
+    "revision": "a217e76d7ed8291e7de8",
+    "url": "/React-FE12/static/js/runtime-main.b02bef0d.js"
+  }
+]);
